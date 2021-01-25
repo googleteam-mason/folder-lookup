750e1702c234390ef6be3bdff2e56611d65531bb
googleteam-mason <77811732+googleteam-mason@users.noreply.github.com>
2021-01-25 09:17:53 -0700

    Update trigger.go

--- trigger.go
+++ trigger.go
@@ -9,9 +9,9 @@ import (
 	"strconv"
 
 	"cloud.google.com/go/bigquery"
-	"github.com/pmenglund/gcp-folders/fetcher"
-	"github.com/pmenglund/gcp-folders/saver"
-	"github.com/pmenglund/gcp-folders/tree"
+	"github.com/googleteam-mason/folder-lookup/fetcher"
+	"github.com/googleteam-mason/folder-lookup/saver"
+	"github.com/googleteam-mason/folder-lookup/tree"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 )
